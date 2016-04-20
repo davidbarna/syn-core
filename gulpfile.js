1bc28a9d38d0504fdac419e47f9a2d9f5d79225a
David Almeida (Soyun) <david.almeida@soyun.es>
2016-04-20 22:02:46 +0200

    Adapted to new syn-dev-tools structure

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,6 @@
  * Gulpfile
  * Tasks are registered from dev-tools module.
  */
-var devTools = require('syn-dev-tools/gulp')
+var devTools = require('syn-dev-tools').gulp
 var manager = devTools.Manager.getInstance(require('gulp'))
 manager.registerTasks()
